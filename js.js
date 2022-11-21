ce33b10c12016479603c10845c0763bbd4358ecd
mohamad-j <58301006+mohamad-j@users.noreply.github.com>
2022-11-21 09:24:21 +0100

    Added js file.

---
+++ js.js
@@ -0,0 +1,8 @@
+/** Variables and Data types */
+
+
+// Type          name of variable        Assign                 Value
+
+    var             name                   =                     "Moha";
+    let             age                   =                       2234 ;
+    const           name                   =                     "Constant";
