864ece91a030f92448960cb51a6ffc278d230e8a
Olexisa18 <bovodessa@gmail.com>
2022-11-21 12:36:32 +0100

    added js.js

---
+++ js.js
@@ -0,0 +1,18 @@
+
+
+/// Variables
+var name = {}
+let arr = [];
+let bool = true;
+let boolf = false;
+let age = 24;
+const year = `2022`;
+
+
+
+////Functions
+function myf1() {}
+myf1();
+
+/////Statments
+console.log
