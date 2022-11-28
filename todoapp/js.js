01e94ac55fcf36534a8cc90abbbcc9a9383588d6
Marcis <117675205+20m1k20@users.noreply.github.com>
2022-11-28 21:13:19 +0100

    Add files via upload

---
+++ todoapp/js.js
@@ -0,0 +1,24 @@
+
+function putInList() {
+    var formcontrol = document.getElementById("exampleFormControlInput1").value;
+    document.getElementById("1.1").innerHTML = formcontrol;
+    var formcontrol1 = document.getElementById("exampleFormControlTextarea1").value;
+    document.getElementById("2.2").innerHTML = formcontrol1;
+    var formcontrol2 = document.getElementById("1").value;
+    document.getElementById("3.3").innerHTML = formcontrol2;
+    var formcontrol3 = document.getElementById("2").value;
+    document.getElementById("4.4").innerHTML = formcontrol3;
+    var formcontrol4 = document.getElementById("3").value;
+    document.getElementById("5.5").innerHTML = formcontrol4;
+    var formcontrol5 = document.getElementById("4").value;
+    document.getElementById("6.6").innerHTML = formcontrol5;
+}
+
+
+
+
+
+
+// document.getElementsByClassName('col')
+// document.getElementById('formdiv')
+// document.createElement) ('')
